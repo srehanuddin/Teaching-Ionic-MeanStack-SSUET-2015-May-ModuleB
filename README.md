fe30f2ed8f1a4665c195547512cf658fa4a594b5
inampaki <inampaki@hotmail.com>
2015-05-17 21:01:24 +0500

    Class 2015_05_17

--- README.md
+++ README.md
@@ -13,3 +13,12 @@
 * Developed Hello World Program
 
 ========================================
+
+###Class 2015-05-17 - TypeScript
+
+* Basic Introduction of TypeScript
+* Configuration of Visual Studio Code for TypeScript Project (http://blogs.msdn.com/b/typescript/archive/2015/04/30/using-typescript-in-visual-studio-code.aspx)
+* Started Chapter 2 of Mastering TypeScript (https://www.dropbox.com/s/iwppl4bxm3a4wwx/Mastering%20TypeScript%20-%20Nathan%20Rozentals.pdf?dl=0)
+* Practice of TypeScript
+
+========================================
---
+++ TypeScript/class 2015_05_17/Demo001/main.js
@@ -0,0 +1,41 @@
+var a = 1;
+a = "asd";
+a = 2;
+var obj = { name: "ABC", id: 1 };
+console.log(obj);
+obj = { name3: "asdas", name2: "asdasd" };
+console.log(obj);
+//obj = { name3 : "asdas",  name2 : "asdasd" }
+/*
+var a:number = 1;
+a : string = "asdas";
+*/
+/*var str : string = a.toString();
+
+var compare : boolean = (1 === 1)
+
+var y : number = 1 + "" + 2;
+
+
+if(a.toString() == "test"){
+    
+}
+
+*/
+/*
+var b = "1";
+
+1
+"1"
+
+if("1" === "Pakistan"){
+    console.log("True");
+} else {
+    console.log("false")
+}
+if(a.toString() === b){
+    console.log("True");
+} else {
+    console.log("false")
+}
+*/
