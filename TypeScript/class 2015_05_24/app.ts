04aad35f98a55c6fc904f994ef723a0e79a2d025
inampaki <inampaki@hotmail.com>
2015-05-24 17:21:06 +0500

    Class 2015_05_24

---
+++ TypeScript/class 2015_05_24/app.ts
@@ -0,0 +1,50 @@
+/*
+var a : any = 1
+a = "asdd";
+a = [1,"sad"]
+
+var numArr : any[] =[1,2,3]
+numArr = [11,22,3];
+numArr = true
+numArr = ["a", "b", 1, {}]
+*/
+/*
+var a:number = 1;
+a = 1;
+a = <any>"asdasd";
+
+
+var obj = { a : 1 }
+obj["a"]
+*/
+
+//var obj = { name : "Zia", age : 25 };
+
+//obj["name1"] = "asdasd";
+/*
+var obj:any = { name : "Zia", age : 25 };
+obj = { name1 : "Rehan", age : 25 };
+obj = { };
+obj = { name1 : 1, age : 25 };
+
+*/
+
+/*
+var obj = { name: "Rehan", class : 1 };
+obj = { name: "Zia", class : 1 };
+obj = <any>{}
+obj = {name  : "z" , class : 2}
+*/
+
+//var a = 1;
+//a = "asdasd";
+
+/*
+function sum (a:number){
+	return a + 2;
+}
+
+alert(sum(1));
+alert(sum(3));
+
+*/
