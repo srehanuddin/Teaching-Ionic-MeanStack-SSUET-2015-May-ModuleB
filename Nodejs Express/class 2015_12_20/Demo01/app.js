7eea52ea57f4a6d3b3651676b9b868dc4d5f57b6
srehanuddin <s.rehanuddin@gmail.com>
2015-12-20 23:05:56 +0500

    class 2015-12-20

---
+++ Nodejs Express/class 2015_12_20/Demo01/app.js
@@ -0,0 +1,31 @@
+console.log("Test");
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test123');
+
+var catSchema = mongoose.Schema({ name: String, name1 : String })
+
+var Cat = mongoose.model('Cat', catSchema);
+
+/*
+var myObj = { _id : 1, name: 'Zildjian' }
+var kitty = new Cat(myObj);
+kitty.save(function (err, data) {
+  if (err) // ...
+  console.log('meow');
+});
+*/
+
+Cat.find({ name : "rehan" }, function(err, data){
+	console.log(err);
+	console.log(data);
+	
+	data[0].name1 = "test";
+	
+	data[0].save()
+	
+	console.log(data[0]);
+	
+	
+});
