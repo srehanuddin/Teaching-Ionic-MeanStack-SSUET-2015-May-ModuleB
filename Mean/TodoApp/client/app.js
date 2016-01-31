e19737f60ec3aebf18fe69c047f9ec97634f3796
srehanuddin <s.rehanuddin@gmail.com>
2016-01-31 17:39:14 +0500

    Test

--- Mean/TodoApp/client/app.js
+++ Mean/TodoApp/client/app.js
@@ -1,4 +1,4 @@
-System.register(["angular2/platform/browser", "angular2/core"], function(exports_1) {
+System.register(["angular2/platform/browser", "angular2/core", 'angular2/http'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var browser_1, core_1;
+    var browser_1, core_1, http_1, http_2;
     var newTask, taskTable, todoApp;
     return {
         setters:[
@@ -17,6 +17,10 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
             },
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+                http_2 = http_1_1;
             }],
         execute: function() {
             newTask = (function () {
@@ -25,6 +29,7 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
                     this.desp = desp;
                     this.status = status;
                     this.show = show;
+                    this.id = Date.now();
                 }
                 newTask.prototype.delete = function () {
                     this.status = false;
@@ -33,20 +38,25 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
             })();
             taskTable = (function () {
                 function taskTable() {
+                    this.deleteEvent = new core_1.EventEmitter();
                 }
                 taskTable.prototype.inactive = function () {
                     this.tasks.status = this.tasks.status ? false : true;
                 };
                 taskTable.prototype.delete = function () {
-                    this.tasks.show = false;
+                    //this.tasks.show = false;
+                    console.log("curTask");
+                    console.log(this.tasks);
+                    this.deleteEvent.emit(this.tasks);
                 };
                 taskTable = __decorate([
                     core_1.Component({
-                        selector: 'tr',
+                        selector: '.myCom',
                         host: {
                             class: 'td'
                         },
                         inputs: ['tasks'],
+                        outputs: ['deleteEvent'],
                         template: "\n\t<td> {{ tasks.task }} </td>\n\t<td> {{ tasks.desp }} </td>\n\t<td><button class=\"ui green button\" *ngIf=\"tasks.status\" (click)=\"inactive()\">Activated</button> <button class=\"ui red button\" *ngIf=\"!tasks.status\"  (click)=\"inactive()\">Unactivated</button></td>\n\t<td><button class=\"ui red button\" (click)=\"delete()\">Remove</button> </td>\t\n\t\n\t"
                     }), 
                     __metadata('design:paramtypes', [])
@@ -55,16 +65,61 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
             })();
             //Main Component
             todoApp = (function () {
-                function todoApp() {
+                function todoApp(http) {
+                    this.http = http;
                     this.tasks = [];
+                    this.getTodos();
                 }
+                todoApp.prototype.getTodos = function () {
+                    var _this = this;
+                    this.loadingTasks = true;
+                    this.http.request('http://localhost:3000/api/todo')
+                        .subscribe(function (res) {
+                        console.log(res.json());
+                        setTimeout(function () {
+                            _this.tasks = res.json();
+                            _this.loadingTasks = false;
+                        }, 1000);
+                    });
+                };
                 todoApp.prototype.addTodo = function (task, desp) {
+                    var _this = this;
                     console.log("task: " + task.value + " description : " + desp.value);
                     var obj = new newTask(task.value, desp.value, true, true);
                     console.log(obj);
-                    this.tasks.push(obj);
-                    task.value = "";
-                    desp.value = "";
+                    var headers = new http_1.Headers();
+                    headers.append('Content-Type', 'application/json');
+                    var opts = new http_1.RequestOptions();
+                    opts.headers = headers;
+                    this.http.post('http://localhost:3000/api/todo', JSON.stringify(obj), opts)
+                        .subscribe(function (res) {
+                        _this.tasks.push(res.json().todo);
+                        task.value = "";
+                        desp.value = "";
+                    });
+                };
+                todoApp.prototype.deleteTask = function (task) {
+                    var _this = this;
+                    this.http.delete('http://localhost:3000/api/todo/' + task.id)
+                        .subscribe(function (res) {
+                        for (var i = 0; i < _this.tasks.length; i++) {
+                            if (_this.tasks[i].id == task.id) {
+                                _this.tasks.splice(i, 1);
+                                break;
+                            }
+                        }
+                    });
+                };
+                todoApp.prototype.makeRequest = function () {
+                    var _this = this;
+                    this.loading = true;
+                    this.http.request('http://jsonplaceholder.typicode.com/posts/1')
+                        .subscribe(function (res) {
+                        console.log(res);
+                        console.log(res.json());
+                        _this.data = res.json();
+                        _this.loading = false;
+                    });
                 };
                 todoApp = __decorate([
                     core_1.Component({
@@ -72,11 +127,11 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
                         directives: [taskTable],
                         templateUrl: 'TodoApp.html',
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [http_1.Http])
                 ], todoApp);
                 return todoApp;
             })();
-            browser_1.bootstrap(todoApp);
+            browser_1.bootstrap(todoApp, [http_2.HTTP_PROVIDERS]);
         }
     }
 });
--- Mean/TodoApp/client/app.ts
+++ Mean/TodoApp/client/app.ts
@@ -1,17 +1,21 @@
 import { bootstrap } from "angular2/platform/browser" ;
-import { Component } from "angular2/core" ;
+import { Component, EventEmitter } from "angular2/core" ;
+import { Http, Response, RequestOptions, Headers } from 'angular2/http';
+import { HTTP_PROVIDERS } from 'angular2/http';
 
 class newTask {
 	task: string;
 	desp: string;
 	status: boolean;
-	show: boolean
+	show: boolean;
+    id : number;
 	
 	constructor(task:string,desp:string,status: boolean,show: boolean){
 		this.task = task;
 		this.desp = desp;
 		this.status = status;
 		this.show = show
+        this.id = Date.now();
 		
 	}
 	delete(){
@@ -22,11 +26,12 @@ class newTask {
 
 
 @Component({
-	selector: 'tr',
+	selector: '.myCom',
 	host:{
 		class: 'td'
 	},
 	inputs: ['tasks'],
+    outputs : ['deleteEvent'],
 	template:`
 	<td> {{ tasks.task }} </td>
 	<td> {{ tasks.desp }} </td>
@@ -37,12 +42,23 @@ class newTask {
 })
 class taskTable {
 	tasks :newTask;
+    
+    deleteEvent : EventEmitter<newTask>;
+    
+    constructor(){
+        this.deleteEvent = new EventEmitter();
+    }
 	
 	inactive(){
 		this.tasks.status = this.tasks.status ? false : true
 	}
 	delete(){
-		this.tasks.show = false;
+		//this.tasks.show = false;
+        
+        console.log("curTask");
+        console.log(this.tasks);
+        
+        this.deleteEvent.emit(this.tasks);
 	}
 	
 }
@@ -58,17 +74,83 @@ class taskTable {
 })
 class todoApp{
 	tasks: newTask[] = [];
+    loadingTasks : boolean;
+    
+    constructor(public http: Http) {
+        
+        this.getTodos();
+    }
+    
+    getTodos(): void {
+        this.loadingTasks = true;
+        this.http.request('http://localhost:3000/api/todo')
+        .subscribe((res: Response) => {
+            console.log(res.json());
+            setTimeout(()=>{
+                this.tasks = res.json();
+                this.loadingTasks = false;
+            }, 1000)
+            
+        });
+    }
 	
 	addTodo(task: HTMLInputElement, desp:HTMLInputElement){
 		console.log(`task: ${task.value} description : ${desp.value}`)
 			var obj = new newTask(task.value,desp.value,true,true)
 			console.log(obj)
-			this.tasks.push(obj);
-			task.value = "";
-			desp.value = "";		
+		
+            let headers: Headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+
+            let opts: RequestOptions = new RequestOptions();
+            opts.headers = headers;
+        	
+            this.http.post('http://localhost:3000/api/todo',
+                JSON.stringify(obj),
+                opts )
+                .subscribe((res: Response) => {
+                     this.tasks.push(res.json().todo);
+                     
+                     task.value = "";
+			         desp.value = "";
+                 });
+    		
 	}
+    
+    deleteTask(task){
+        
+        this.http.delete('http://localhost:3000/api/todo/' + task.id)
+                .subscribe((res: Response) => {
+                     
+                     for(var i = 0; i < this.tasks.length; i++){
+                        if(this.tasks[i].id == task.id){
+                            this.tasks.splice(i, 1);                
+                            break;
+                        }
+                    }
+                    
+                     
+                 });
+    }
+    
+    
+    
+    
+    data: Object;
+    loading: boolean;
+    makeRequest(): void {
+        this.loading = true;
+        this.http.request('http://jsonplaceholder.typicode.com/posts/1')
+        .subscribe((res: Response) => {
+            
+            console.log(res)
+            console.log( res.json())
+            this.data = res.json();
+            this.loading = false;
+        });
+    }
 	
 }
 
 
-bootstrap(todoApp)
+bootstrap(todoApp, [HTTP_PROVIDERS])
